18b0be279db7ccd4658b5bc5fd8cbcc200d4fa15
Iglesias Fourcade <raul4cade@gmail.com>
2025-12-03 14:54:08 +0100

    Change build script

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,5 @@ import type { Environment } from '../app/types/environment';
 
 export const environment: Environment = {
   apiUrl: 'http://localhost:8080/bookmarks',
+  production: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ import type { Environment } from '../app/types/environment';
 
 export const environment: Environment = {
   apiUrl: 'https://bookmarks-server.raulif.deno.net/bookmarks',
+  production: true
 };
